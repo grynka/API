bb81cb9289d8786f0a6042fb8ed0da2d2670c788
grynka <ihor.kozhemyakin@gmail.com>
2023-03-21 13:48:22 +0200

    Update sendEmail.js

--- helpers/sendEmail.js
+++ helpers/sendEmail.js
@@ -1,8 +1,6 @@
 const sgMail = require('@sendgrid/mail')
 
-const { SENDGRID_KEY } = process.env;
-
-sgMail.setApiKey(SENDGRID_KEY)
+sgMail.setApiKey(process.env.SENDGRID_KEY)
 
 const sendEmail = async (data) => {
     const email = {...data, from: "ihor.kozhemyakin@gmail.com"}
